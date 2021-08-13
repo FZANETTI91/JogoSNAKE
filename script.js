2ce704e32f9f810194dab374d1f658b012a5c3ba
FZANETTI91 <fzanettifilho@gmail.com>
2021-08-13 11:03:52 -0300

    Criacao inicial das comidas da Snake

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)* box,
+    y: Math.floor(Math.random() * 15 + 1)* box
+
+}
 
 
 
@@ -21,6 +26,12 @@ function criarSnake(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+
+function drawFood(){
+    context.fillStyle = "Red"
+    context.fillRect(food.x, food.y, box, box);
+}
+
 
 document.addEventListener("keydown", update);
 
@@ -39,6 +50,7 @@ function iniciarJogo(){
 
     criarBG();
     criarSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
