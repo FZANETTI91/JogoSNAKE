b11b72252923243c1c2ac2d01a87bc1c2ea7530c
FZANETTI91 <fzanettifilho@gmail.com>
2021-08-13 10:31:27 -0300

    Definir Movimento Snake

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,5 +22,32 @@ function criarSnake(){
     }
 }
 
-criarBG();
+function iniciarJogo(){
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    
+    snake.unshift(newHead);
+
+}
+
+/*criarBG();
 criarSnake();
+
+##funções do jogo para iniciar certinho##*/
+
+let jogo = setInterval(iniciarJogo, 1000);
